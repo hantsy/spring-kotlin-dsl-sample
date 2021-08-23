489edb507cdb53f07add14715fef9928dbde6902
hantsy <hantsy@gmail.com>
2021-08-23 10:36:31 +0800

    chore: clean codes

--- webmvc/src/test/kotlin/com/example/demo/ApplicationTests.kt
+++ webmvc/src/test/kotlin/com/example/demo/ApplicationTests.kt
@@ -1,22 +1,22 @@
 package com.example.demo
 
 
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
-import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.servlet.function.RouterFunction
-import org.springframework.web.servlet.function.ServerResponse
 
 
-@SpringBootTest(classes = arrayOf(DemoApplication::class),
-        properties = ["context.initializer.classes=com.example.demo.TestConfigInitializer"])
+@SpringBootTest(
+    classes = [DemoApplication::class],
+    properties = ["context.initializer.classes=com.example.demo.TestConfigInitializer"]
+)
 class ApplicationTests {
 
     @Autowired
@@ -34,12 +34,16 @@ class ApplicationTests {
     @Test
     fun `get all posts`() {
         mockMvc
-                .get("/posts")
-                .andExpect {
-                    status {
-                        isOk
-                    }
+            .get("/posts")
+            .andExpect {
+                status {
+                    isOk()
                 }
+                content {
+                    contentType(MediaType.APPLICATION_JSON)
+                }
+                jsonPath("length()", Matchers.greaterThan(0))
+            }
     }
 
 }
